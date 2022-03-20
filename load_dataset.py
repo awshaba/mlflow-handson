cebdca4c6715c3c7002b320b19df7fe3ffc63093
awshaba <101735445+awshaba@users.noreply.github.com>
2022-03-20 20:17:56 +0100

    Removes logging in load_dataset

--- load_dataset.py
+++ load_dataset.py
@@ -1,4 +1,3 @@
-import mlflow
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -10,11 +9,6 @@ def load_dataset(test_size=0.2, random_state=123, download_path='.'):
     # Process dataset
     X = iris.data
     y = iris.target
-    # Log dataset parameters
-    mlflow.log_param("dataset_path", download_path)
-    mlflow.log_param("dataset_shape", X.shape)
-    mlflow.log_param("test_size", test_size)
-    mlflow.log_param("random_state", random_state)
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 
