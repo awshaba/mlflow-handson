7590cd334036355af58289eaf1491e1d5e7c9173
awshaba <101735445+awshaba@users.noreply.github.com>
2022-03-29 21:36:55 +0200

    updating the dataset

--- load_dataset.py
+++ load_dataset.py
@@ -5,10 +5,10 @@ from sklearn.model_selection import train_test_split
 
 def load_dataset(test_size=0.2, random_state=123, download_path='.'):
     # Load dataset
-    iris = datasets.load_iris()
+    dataset = datasets.load_breast_cancer()
     # Process dataset
-    X = iris.data
-    y = iris.target
+    X = dataset.data
+    y = dataset.target
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 
