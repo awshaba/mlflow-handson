186798443497b171c622585e476ff8b4c0871551
awshaba <101735445+awshaba@users.noreply.github.com>
2022-03-29 22:28:34 +0200

    Add more metrics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from catboost import CatBoostClassifier
 from load_dataset import load_dataset
-from sklearn.metrics import accuracy_score, log_loss
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 if __name__ == '__main__':
     x_train, x_test, y_train, y_test = load_dataset()
@@ -14,4 +14,6 @@ if __name__ == '__main__':
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
     acc = accuracy_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
     print(f"Accuracy {acc}")
