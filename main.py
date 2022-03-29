2d4f916094ec093b9000dc9d9b6c066fa2bddcaa
awshaba <101735445+awshaba@users.noreply.github.com>
2022-03-29 21:39:55 +0200

    Update the model

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-import xgboost as xgb
+from catboost import CatBoostClassifier
 from load_dataset import load_dataset
 from sklearn.metrics import accuracy_score, log_loss
 
 if __name__ == '__main__':
     x_train, x_test, y_train, y_test = load_dataset()
-    dtrain = xgb.DMatrix(x_train, label=y_train)
-    dtest = xgb.DMatrix(x_test, label=y_test)
     params = {
-        "objective": "multi:softprob",
-        "num_class": 3,
-        "learning_rate": 0.3,
-        "eval_metric": "mlogloss",
-        "colsample_bytree": 1.0,
-        "subsample": 1.0,
-        "seed": 42,
+    "iterations": 2,
+    "learning_rate": 1,
+    "depth": 2,
+    "allow_writing_files": False,
     }
-    
-    model = xgb.train(params, dtrain, evals=[(dtrain, "train")])
-    y_proba = model.predict(dtest)
-    y_pred = y_proba.argmax(axis=1)
-    loss = log_loss(y_test, y_proba)
+    model = CatBoostClassifier(**params)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
     acc = accuracy_score(y_test, y_pred)
-        
+    print(f"Accuracy {acc}")
